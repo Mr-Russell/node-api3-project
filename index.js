6e88acee00e992e212c1fed53513de520a42b273
Brian Russell <bruss@brians-mbp.lan>
2020-06-04 16:15:40 -0400

    Added dotenv dependency for Deployment Stretch Goal

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // code away!
+require('dotenv').config()
+
 const server = require('./server.js');
 
 const port = process.env.PORT || 4001
+
+server.get("/", (req, res) => {
+  res.status(200).json({
+    port: `API Deployed on Port ${process.env.PORT}`,
+    greeting: process.env.GREET
+  })
+})
 
 server.listen(port, () => {
   console.log(`\n* Server Running on port ${port} *\n`);
