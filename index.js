1fabab53df106bfdf0583829218f6a3cd1812daf
Brian Russell <bruss@brians-mbp.lan>
2020-06-04 16:01:50 -0400

    Updated for deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require('./server.js');
 
-const port = 4001
+const port = process.env.PORT || 4001
 
 server.listen(port, () => {
   console.log(`\n* Server Running on port ${port} *\n`);
